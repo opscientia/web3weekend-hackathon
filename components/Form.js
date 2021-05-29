494683fd5047a892a274df4f7e3dfaa0b23a4928
McCarroll <amca@c02r2decg8wn.home>
2021-05-29 21:35:20 +0100

    form fit

--- components/Form.js
+++ components/Form.js
@@ -4,35 +4,28 @@ import { Input, Button, Row, Col, Loading } from '@geist-ui/react';
 function Form(props) {
     return (
         <div align="center">
-        <Row gap={.8} style={{ marginTop: '15px', marginBottom: '15px' }}>
-        <Col>
-            <Input placeholder="Enter here" >
-                Title: 
-            </Input>
-        </Col>
-        <Col>
-            <Input placeholder="Enter here" >
-                Authors: 
-            </Input>
-        </Col>
-        <Col>
-            <Input placeholder="Enter here" >
-                Tags:
-            </Input>
-        </Col>
-      </Row>
-      {!(props.loading == null) && <div>
-                <Row style={{ margin: '10px'}}>
-                    <Loading>{props.loading}</Loading>
-                </Row>
-            </div>
-            }
             <div>
                 {props.listItems}
             </div>
-      <Button align="center">Submit</Button>
+            <Row gap={.8} style={{ marginTop: '15px', marginBottom: '15px' }}>
+            <Col>
+                <Input placeholder="Enter here" >
+                    Title: 
+                </Input>
+            </Col>
+            <Col>
+                <Input placeholder="Enter here" >
+                    Authors: 
+                </Input>
+            </Col>
+            <Col>
+                <Input placeholder="Enter here" >
+                    Tags:
+                </Input>
+            </Col>
+            </Row>
+            <Button align="center">Submit</Button>
         </ div>
-
     )
 };
 
